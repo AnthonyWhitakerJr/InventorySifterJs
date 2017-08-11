27f001ed5dc6318073209e476e89f6672b1578dc
Anthony Whitaker <AnthonyWhitakerJr@gmail.com>
2017-08-11 19:02:31 -0400

    Setup project

---
+++ src/main/java/whitaker/anthony/InventorySifterApplication.java
@@ -0,0 +1,12 @@
+package whitaker.anthony;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventorySifterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventorySifterApplication.class, args);
+	}
+}
