a86839a175b7d5b05c64df0e10a739c7e73a4d2e
Anthony Whitaker <AnthonyWhitakerJr@gmail.com>
2017-08-16 07:28:12 -0400

    Add CI & Code Coverage

--- src/main/java/whitaker/anthony/InventorySifterApplication.java
+++ src/main/java/whitaker/anthony/InventorySifterApplication.java
@@ -4,11 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import whitaker.anthony.generator.ProductGenerator;
 import whitaker.anthony.model.Product;
 import whitaker.anthony.repository.ProductRepository;
-
-import java.time.LocalDate;
 
 @SpringBootApplication
 public class InventorySifterApplication implements CommandLineRunner {
@@ -22,7 +19,7 @@ public class InventorySifterApplication implements CommandLineRunner {
 
   @Override
   public void run(String... strings) throws Exception {
-    productRepository.save(new Product("12346567", "Apple, Fuji", "Produce", ProductGenerator.getRandomDate(LocalDate.now(), LocalDate.now().plusMonths(2))));
+//    productRepository.save(new Product("12346567", "Apple, Fuji", "Produce", ProductGenerator.getRandomDate(LocalDate.now(), LocalDate.now().plusMonths(2))));
 
     System.out.println("Products found with findAll():");
     System.out.println("-------------------------------");
