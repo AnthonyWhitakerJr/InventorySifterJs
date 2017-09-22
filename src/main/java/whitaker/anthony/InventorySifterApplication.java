bd21ad196e6408cb258da9019cb6a4c654d5ef5e
Anthony Whitaker <AnthonyWhitakerJr@gmail.com>
2017-09-22 13:55:10 -0400

    Test application run

--- src/main/java/whitaker/anthony/InventorySifterApplication.java
+++ src/main/java/whitaker/anthony/InventorySifterApplication.java
@@ -4,11 +4,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import whitaker.anthony.generator.DataGenerator;
 import whitaker.anthony.model.Product;
 import whitaker.anthony.repository.ProductRepository;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static whitaker.anthony.generator.DataGenerator.*;
+
 @SpringBootApplication
 public class InventorySifterApplication implements CommandLineRunner {
+
+	private static final String FILENAME_DATASET_1 = "src/test/resources/dataset1.txt";
 
 	@Autowired
 	private ProductRepository productRepository;
@@ -19,8 +27,14 @@ public class InventorySifterApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... strings) throws Exception {
-//    productRepository.save(new Product("12346567", "Apple, Fuji", "Produce", ProductGenerator.getRandomDate(LocalDate.now(), LocalDate.now().plusMonths(2))));
+		productRepository.deleteAll();
 
+		List<Product> products = new ArrayList<>();
+
+		// Add first data set.
+		products.addAll(DataGenerator.parseProductsFromFile(FILENAME_DATASET_1, DEFAULT_DELIMITER, DEFAULT_EXPIRATION_DATE_FORMAT, DEFAULT_EXPIRATION_DATE_LOCALE));
+
+		productRepository.save(products);
 		System.out.println("Products found with findAll():");
 		System.out.println("-------------------------------");
 		for(Product product : productRepository.findAll()) {
